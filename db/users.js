62b5b106d7e77b2d91fe0c52da84593254aa0168
Vasco Marques <vasco_marques_ei@hotmail.com>
2017-07-01 23:08:33 +0100

    sadsad
    asdsa

--- db/users.js
+++ db/users.js
@@ -2,6 +2,7 @@ var records = [
     { id: 1, username: 'pedro', password: '123', displayName: 'PedroMatos', emails: [ { value: 'pedromatos@outlook.pt' } ] }
   , { id: 2, username: 'paulo', password: '123', displayName: 'teste', emails: [ { value: 'teste@example.com' } ] }
   , { id: 3, username: 'vasco', password: '123', displayName: 'teste', emails: [ { value: 'teste@example.com' } ] }
+  , { id: 4, username: 'grupo1', password: '123', displayName: 'grupo1', emails: [ { value: 'grupo1@example.com' } ] }
 ];
 
 exports.findById = function(id, cb) {
